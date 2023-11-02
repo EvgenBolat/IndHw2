1b2cbd43c348a701626066aaf786bae14de6e870
Хавронич Евгений <145042167+EvgenBolat@users.noreply.github.com>
2023-11-02 14:08:07 +0300

    Update WithTheInput.asm

--- Files/RISKFILES/WithTheInput.asm
+++ Files/RISKFILES/WithTheInput.asm
@@ -3,7 +3,7 @@
 .text
 	addi 	sp, sp, -12
 	sw	ra, 8(sp)
-	print_str("Please, input the accurancy: ") #first arg – string to be output to the console. Returns nothing.
+	print_str("Please, input the accurancy in the range from 0.001 to 0.00000001: ") #first arg – string to be output to the console. Returns nothing.
 	read_double(fa0) # first arg – an register for answer returning. Returns an double number in the register of first argument
 	jal FindX #first arg – an accuracy, that we should check if it contains in the range [0.001;0.00000001]. 
 		  #Returns the equation root of x^4 - x^3 - 2.5 = 0 in the fa0 or stopping the program if the accuracy isn't in the range.
